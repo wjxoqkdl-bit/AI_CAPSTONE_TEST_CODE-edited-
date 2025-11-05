1397d2adc4c43e2385285fec595074be94bb8765
apinball <apinball91@gmail.com>
2025-11-06 03:48:20 +0900

    Fix: Correct STATICFILES_DIRS path

--- AICapstone/base.py
+++ AICapstone/base.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, 'frontend', 'static'),
+    os.path.join(BASE_DIR, 'frontend', 'static'),
 ]
 
 # Default primary key field type
