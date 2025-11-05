2ce86efcdda92ee5992e97414714e391d3c0e7a9
apinball <apinball91@gmail.com>
2025-11-06 08:44:39 +0900

    HOTFIX: re9 config

--- AICapstone/base.py
+++ AICapstone/base.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'static'),
--- AICapstone/production.py
+++ AICapstone/production.py
@@ -5,8 +5,8 @@ from .base import *
 # 배포 환경 설정
 DEBUG = False
 
-# 실제 서비스할 도메인 및 Elastic Beanstalk 호스트를 이곳에 추가합니다.
-ALLOWED_HOSTS = ['aicapstone-dev.ap-northeast-2.elasticbeanstalk.com', '.elasticbeanstalk.com', 'localhost', '127.0.0.1']
+# 모든 호스트를 허용하여 502 오류의 원인이 ALLOWED_HOSTS인지 확인합니다.
+ALLOWED_HOSTS = ['*']
 
 # static files
 STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
