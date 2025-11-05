3c5b638a5a0a0423d86838ee90c74ad070411f65
apinball <apinball91@gmail.com>
2025-11-05 23:02:51 +0900

    feat(deploy): Add configuration for AWS Elastic Beanstalk

--- AICapstone/AICapstone/production.py
+++ AICapstone/AICapstone/production.py
@@ -5,6 +5,6 @@ from .base import *
 # 배포 환경 설정
 DEBUG = False
 
-# 실제 서비스할 도메인을 이곳에 추가해야 합니다.
-# 예: ALLOWED_HOSTS = ['mydomain.com']
-ALLOWED_HOSTS = []
+# 실제 서비스할 도메인 및 Elastic Beanstalk 호스트를 이곳에 추가합니다.
+ALLOWED_HOSTS = ['.elasticbeanstalk.com']
+
