487759ca8275834b1ed22d4babb4858e7f1fa38a
apinball <apinball91@gmail.com>
2025-11-06 09:18:38 +0900

    HOTFIX: re15 config

--- AICapstone/base.py
+++ AICapstone/base.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "static",
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
